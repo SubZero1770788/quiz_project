c36f463089dbd54e29a6c0d94227e9434b09aa34
SubZero1770788 <djanuszkiewicz147@gmail.com>
2025-07-21 01:29:52 +0200

    Finished

---
+++ Database/Migrations/20250720232939_Added index tracking.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace quiz_project.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addedindextracking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AnswerStates_OnGoingQuizStates_OnGoingQuizStateId",
+                table: "AnswerStates");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AnswerStates_OnGoingQuizStates_OnGoingQuizStateId",
+                table: "AnswerStates",
+                column: "OnGoingQuizStateId",
+                principalTable: "OnGoingQuizStates",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AnswerStates_OnGoingQuizStates_OnGoingQuizStateId",
+                table: "AnswerStates");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AnswerStates_OnGoingQuizStates_OnGoingQuizStateId",
+                table: "AnswerStates",
+                column: "OnGoingQuizStateId",
+                principalTable: "OnGoingQuizStates",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
